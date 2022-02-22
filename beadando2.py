ce985e187e5c3b57091f34a506dd3c69c6e1a0a6
Balázs Jónás <jonaasbalaazs@gmail.com>
2022-02-22 20:15:50 +0100

    2. beadandó

--- beadando2.py
+++ beadando2.py
@@ -1,15 +1,17 @@
 def is_perfect(n: int) -> bool:
-    def factor(n: int) -> list:
-        factors = [1]
-        rem  = n
-        for i in range(2,n+1):
-            if rem % i == 0:
-                rem = rem / i
-                factors.append(i)
-        return factors
-    factors = factor(n)
-    return sum(factors) == n
+    def get_divisors(n: int) -> list:
+        divisors = [1]
+        for i in range(2, n):
+            if n % i == 0:
+                divisors.append(i)
+        return divisors
+    divisors = get_divisors(n)
+    return sum(divisors) == n
 
+
+assert is_perfect(6)
+assert is_perfect(28)
+assert is_perfect(496)
 
 for i in range(1,10):
     if is_perfect(i):
