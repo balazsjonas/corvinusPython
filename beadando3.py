11a1762403c96adf6844bea4512e1a986e0a77d4
Balázs Jónás <jonaasbalaazs@gmail.com>
2022-03-08 18:38:40 +0100

    3. beadandó

--- beadando3.py
+++ beadando3.py
@@ -2,27 +2,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-open_air = pd.read_csv("data/input/openair.csv")
-open_air['date'] = pd.to_datetime(open_air['date'])
-open_air.set_index('date', inplace=True)
-open_air['weekday'] = open_air.index.weekday
-open_air.groupby('weekday').aggregate(sum)
-open_air.groupby('weekday').aggregate(np.median)
-HÉT_MAGYARUL =  ['Hétfő', 'Kedd', 'Szerda', 'Csütörtök', 'Péntek', 'Szombat', 'Vasárnap']
+na_values = ["NoData", "InVld", "Maintain", "Samp<", "Span", "ZRef", "Zero"]
+names = [
+    "Date", "Time", "SO2", "NO", "NO2", "CO", "PM10", "Szels", "Szelir", "Homers",
+    "Parat", "Legny", "Naps", "NOx", "PM25", "O3"
+]
 
-print(df.info)
-data = []
-for c in df.columns[1:]: # kihagyjuk a dátum oszlopot, mivel ott nincs értelme az átlagnak
-    se = df[c]
-    data.append({"column": c, "min": se.min(), "max": se.max(), "mean": se.mean()})
+path = "data/input/legszenny/"
+file_names_metro_1 = ['gyor1_2011.csv',
+                      'gyor1_2012.csv'
+                      ]
+file_names_metro_2 = ['gyor2_2011.csv', 'gyor2_2012.csv']
+df = pd.DataFrame()
+for file in file_names_metro_1:
+    skiprows = [i for i, l in enumerate(open(path + file)) if not l[0].isdigit()]
+    tmp = pd.read_csv(path + file,
+                      skiprows=skiprows,
+                      names=names,
+                      na_values=na_values,
+                      low_memory=False,
+                      decimal=','
+                      )
+    df = pd.concat([df, tmp])
 
-stats = pd.DataFrame(data)
-print(stats)
-print(df["co"].describe())
-print(df.describe().info)
-
-df["co"][:168].plot.line()
-
-graf=df["co"][:168]
-graf.plot()
+# df.groupby('Time').aggregate(np.mean).plot(kind='box')
+# df.groupby('Time')['CO', 'PM10'].aggregate(np.median)
+df.groupby('Time').aggregate(np.median)[['CO', 'PM10']].plot()  # ?????????
 plt.show()
