94c6fc7c5276b680dbd6edbc62681f9852abc102
Balazs Jonas <Balazs_Jonas@mentor.com>
2022-03-09 17:24:20 +0100

    4. beadandó

--- beadando4.py
+++ beadando4.py
@@ -27,3 +27,31 @@ soft_drinks = canned.groupby('choice_description').aggregate(sum)[['quantity']]
 print(soft_drinks)
 soft_drinks.plot(kind='barh')
 plt.show()
+
+# 2. rész
+import urllib
+adsl_filename = 'http://web.uni-corvinus.hu/~fszabina/data/adls_b.txt' # TODO
+adsl_filename = 'feladat4/adls_b.txt'
+# adsl_columns = 'Start time          	End time            	Activity'
+# sensors_filename = 'http://web.uni-corvinus.hu/~fszabina/data/sensors_b.txt'
+sensors_filename = 'feladat4/sensors_b.txt'
+
+# a tevékenységre fordított összidő alapján csökkenő sorrendben kiírja az alany tevékenységeit!
+adsl = pd.read_csv(adsl_filename, sep=';')
+adsl['Starttime'] = pd.to_datetime(adsl['Starttime'])
+adsl['Endtime'] = pd.to_datetime(adsl['Endtime'])
+adsl['Duration'] = adsl['Endtime'] - adsl['Starttime']
+adsl.groupby('Activity').sum().sort_values('Duration', ascending=False)
+
+# adsl2 = pd.read_csv(adsl_filename, sep='\s*\t', engine='python', skiprows=2)
+# adsl2[:,0] = pd.to_datetime(adsl2[:,0])
+# adsl2[:,1] = pd.to_datetime(adsl2[:,1])
+# adsl2['Duration'] = adsl2[1] - adsl2[0]
+
+# b) sensors
+sensors = pd.read_csv(sensors_filename, sep=';')
+sensors['Starttime'] = pd.to_datetime(sensors['Starttime'])
+sensors['Endtime'] = pd.to_datetime(sensors['Endtime'])
+
+limit = pd.to_datetime('2012.11.15 23:59:59')
+print(sensors[(sensors['Endtime']<=limit) & (sensors['Place'] == 'Bathroom')])
