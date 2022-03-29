9e3e526dd58ef514b99ae3ddd174306d2e00c81c
Balázs Jónás <jonaasbalaazs@gmail.com>
2022-03-29 19:11:32 +0200

    comments

--- beadando4.py
+++ beadando4.py
@@ -2,38 +2,39 @@ import pandas as pd
 import pylab as plt
 
 
-# 1. Rendelések
-# a) adat betöltése
+print('1. Rendelések\n')
+print('a) adatok betöltése')
 df = pd.read_csv('feladat4/orders.tsv', sep='\t')
 
-# b) item_price típus javítása
+print('b) item_price típus javítása')
 df['item_price'] = df['item_price'].str.replace('$', '', regex=False).apply(lambda x: float(x))
 
-# c) rendelések összege
+print('c) rendelések összege')
 df['Sum'] = df['quantity'] *df['item_price']
 
-# d) átlagos rendelés értéke
-print('Az átlagos rendelés értéke ${:.2f}'.format(df['Sum'].mean()))
+print('d) átlagos rendelés értéke')
+print('Az átlagos rendelés értéke: ${:.2f}'.format(df['Sum'].mean()))
 
-# e-f) az adott ételből hányat adtak el, rendezve
+print('e-f) az adott ételből hányat adtak el, rendezve')
 df.groupby('item_name').sum().sort_values(by='quantity')[['quantity']]
 
-# g) dobozos italok
+print('g) dobozos italok')
 canned_names = ['Canned Soda', 'Canned Soft Drink']
 canned = df[(df['item_name'] == canned_names[0]) | ( df['item_name'] == canned_names[1])]
 
-# h) az egyes üdítőkből összesen mennyit rendeltek
+print('# h) az egyes üdítőkből összesen mennyit rendeltek')
 soft_drinks = canned.groupby('choice_description').aggregate(sum)[['quantity']]
 print(soft_drinks)
 soft_drinks.plot(kind='barh')
+plt.title('Rendelések száma üdítőnként')
 plt.show()
 
+
 # 2. rész
+print('\n2. rész\n')
 import urllib
 adsl_filename = 'http://web.uni-corvinus.hu/~fszabina/data/adls_b.txt' # TODO
 adsl_filename = 'feladat4/adls_b.txt'
-# adsl_columns = 'Start time          	End time            	Activity'
-# sensors_filename = 'http://web.uni-corvinus.hu/~fszabina/data/sensors_b.txt'
 sensors_filename = 'feladat4/sensors_b.txt'
 
 # a tevékenységre fordított összidő alapján csökkenő sorrendben kiírja az alany tevékenységeit!
@@ -41,6 +42,8 @@ adsl = pd.read_csv(adsl_filename, sep=';')
 adsl['Starttime'] = pd.to_datetime(adsl['Starttime'])
 adsl['Endtime'] = pd.to_datetime(adsl['Endtime'])
 adsl['Duration'] = adsl['Endtime'] - adsl['Starttime']
+
+print('Tevékenyégekre fordított idő csökkenő sorrendben')
 adsl.groupby('Activity').sum().sort_values('Duration', ascending=False)
 
 # adsl2 = pd.read_csv(adsl_filename, sep='\s*\t', engine='python', skiprows=2)
@@ -54,4 +57,6 @@ sensors['Starttime'] = pd.to_datetime(sensors['Starttime'])
 sensors['Endtime'] = pd.to_datetime(sensors['Endtime'])
 
 limit = pd.to_datetime('2012.11.15 23:59:59')
+
+print('Fürdőszobában töltött időszakok')
 print(sensors[(sensors['Endtime']<=limit) & (sensors['Place'] == 'Bathroom')])
