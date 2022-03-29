2e491ca589146d941a10f5a81195c0ce90864c48
Balázs Jónás <jonaasbalaazs@gmail.com>
2022-03-29 19:45:34 +0200

    4. beadando.

--- beadando4.py
+++ beadando4.py
@@ -4,7 +4,7 @@ import pylab as plt
 
 print('1. Rendelések\n')
 print('a) adatok betöltése')
-df = pd.read_csv('feladat4/orders.tsv', sep='\t')
+df = pd.read_csv('orders.tsv', sep='\t')
 
 print('b) item_price típus javítása')
 df['item_price'] = df['item_price'].str.replace('$', '', regex=False).apply(lambda x: float(x))
@@ -22,7 +22,7 @@ print('g) dobozos italok')
 canned_names = ['Canned Soda', 'Canned Soft Drink']
 canned = df[(df['item_name'] == canned_names[0]) | ( df['item_name'] == canned_names[1])]
 
-print('# h) az egyes üdítőkből összesen mennyit rendeltek')
+print('h) az egyes üdítőkből összesen mennyit rendeltek')
 soft_drinks = canned.groupby('choice_description').aggregate(sum)[['quantity']]
 print(soft_drinks)
 soft_drinks.plot(kind='barh')
@@ -32,27 +32,25 @@ plt.show()
 
 # 2. rész
 print('\n2. rész\n')
-import urllib
-adsl_filename = 'http://web.uni-corvinus.hu/~fszabina/data/adls_b.txt' # TODO
-adsl_filename = 'feladat4/adls_b.txt'
-sensors_filename = 'feladat4/sensors_b.txt'
 
 # a tevékenységre fordított összidő alapján csökkenő sorrendben kiírja az alany tevékenységeit!
-adsl = pd.read_csv(adsl_filename, sep=';')
+adsl_filename = 'http://web.uni-corvinus.hu/~fszabina/data/adls_b.txt'
+adsl = pd.read_csv(adsl_filename, sep='\s*\t', engine='python', skiprows=0)
+adsl.drop(0, inplace=True)
+adsl.columns = ['Starttime', 'Endtime', 'Activity']
 adsl['Starttime'] = pd.to_datetime(adsl['Starttime'])
 adsl['Endtime'] = pd.to_datetime(adsl['Endtime'])
 adsl['Duration'] = adsl['Endtime'] - adsl['Starttime']
-
 print('Tevékenyégekre fordított idő csökkenő sorrendben')
 adsl.groupby('Activity').sum().sort_values('Duration', ascending=False)
 
-# adsl2 = pd.read_csv(adsl_filename, sep='\s*\t', engine='python', skiprows=2)
-# adsl2[:,0] = pd.to_datetime(adsl2[:,0])
-# adsl2[:,1] = pd.to_datetime(adsl2[:,1])
-# adsl2['Duration'] = adsl2[1] - adsl2[0]
 
 # b) sensors
-sensors = pd.read_csv(sensors_filename, sep=';')
+
+sensors_filename = 'http://web.uni-corvinus.hu/~fszabina/data/sensors_b.txt'
+sensors = pd.read_csv(sensors_filename, sep='\t+', engine='python')
+sensors.columns = ['Starttime', 'Endtime', 'Location', 'Type', 'Place']
+sensors.drop(0, inplace=True)
 sensors['Starttime'] = pd.to_datetime(sensors['Starttime'])
 sensors['Endtime'] = pd.to_datetime(sensors['Endtime'])
 
